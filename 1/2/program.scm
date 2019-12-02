df25510b91a502dc8f0c5e672b2f3a0d783635f9
Patrik Billgren <patrik.billgren@telia.no>
2019-12-02 23:38:37 +0100

    Day 1 part 2 in scheme

---
+++ 1/2/program.scm
@@ -0,0 +1,18 @@
+(define filename "input.scm")
+(define in (open-input-file filename))
+(define data (read in))
+(close-input-port in)
+
+(define (mass-to-fuel mass)
+  (- (floor (/ mass 3)) 2))
+
+(define (sum numbers)
+  (if (null? numbers) 0
+    (+ (car numbers) (sum (cdr numbers)))))
+
+(define (calc-fuel mass)
+  (if (<=  mass 0) 0
+    (+ (max (mass-to-fuel mass) 0) (calc-fuel (mass-to-fuel mass)))))
+
+(display (sum (map (lambda (num)
+	      (calc-fuel num)) data)) (current-output-port))
