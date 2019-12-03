bb095cd10cca8fda2532bea83a995c0bd66747f1
Patrik Billgren <patrik.billgren@telia.no>
2019-12-03 07:27:07 +0100

    Refactoring

--- 3/1/program.py
+++ 3/1/program.py
@@ -1,4 +1,3 @@
-
 lines = [line.rstrip('\n') for line in open('input')]
 directions1 = lines[0].split(",")
 directions2 = lines[1].split(",")
@@ -6,37 +5,24 @@ directions2 = lines[1].split(",")
 points1 = set()
 points2 = set()
 
-x = 0
-y = 0
-for direct in directions1:
-    direction, steps = direct[:1], int(direct[1:])
-    for i in range(steps):
-        if direction == 'U':
-            y += 1
-        elif direction == 'D':
-            y -= 1
-        elif direction == "L":
-            x -= 1
-        elif direction == "R":
-            x += 1
-        points1.add((x, y))
+def calc(directions, points):
+    x = 0
+    y = 0
+    for direct in directions:
+        direction, steps = direct[:1], int(direct[1:])
+        for i in range(steps):
+            if direction == 'U':
+                y += 1
+            elif direction == 'D':
+                y -= 1
+            elif direction == "L":
+                x -= 1
+            elif direction == "R":
+                x += 1
+            points.add((x, y))
 
-x = 0
-y = 0
-for direct in directions2:
-    direction, steps = direct[:1], int(direct[1:])
-    for i in range(steps):
-        if direction == 'U':
-            y += 1
-        elif direction == 'D':
-            y -= 1
-        elif direction == "L":
-            x -= 1
-        elif direction == "R":
-            x += 1
-        points2.add((x, y))
-
-print(points1.intersection(points2))
+calc(directions1, points1)
+calc(directions2, points2)
 
 intersection = points1.intersection(points2)
 
--- 3/2/program.py
+++ 3/2/program.py
@@ -5,46 +5,31 @@ directions2 = lines[1].split(",")
 
 points1 = set()
 points2 = set()
-
-x = 0
-y = 0
-length = 0
 pointsToLength1 = dict()
-for direct in directions1:
-    direction, steps = direct[:1], int(direct[1:])
-    for i in range(steps):
-        length += 1
-        if direction == 'U':
-            y += 1
-        elif direction == 'D':
-            y -= 1
-        elif direction == "L":
-            x -= 1
-        elif direction == "R":
-            x += 1
-        points1.add((x, y))
-        if pointsToLength1.get((x,y)) == None:
-            pointsToLength1[(x,y)] = length
-
-x = 0
-y = 0
-length = 0
 pointsToLength2 = dict()
-for direct in directions2:
-    direction, steps = direct[:1], int(direct[1:])
-    for i in range(steps):
-        length += 1
-        if direction == 'U':
-            y += 1
-        elif direction == 'D':
-            y -= 1
-        elif direction == "L":
-            x -= 1
-        elif direction == "R":
-            x += 1
-        points2.add((x, y))
-        if pointsToLength2.get((x,y)) == None:
-            pointsToLength2[(x,y)] = length
+
+def calc(directions, points, pointsToLength):
+    x = 0
+    y = 0
+    length = 0
+    for direct in directions:
+        direction, steps = direct[:1], int(direct[1:])
+        for i in range(steps):
+            length += 1
+            if direction == 'U':
+                y += 1
+            elif direction == 'D':
+                y -= 1
+            elif direction == "L":
+                x -= 1
+            elif direction == "R":
+                x += 1
+            points.add((x, y))
+            if pointsToLength.get((x,y)) == None:
+                pointsToLength[(x,y)] = length
+
+calc(directions1, points1, pointsToLength1)
+calc(directions2, points2, pointsToLength2)
 
 intersection = points1.intersection(points2)
 
